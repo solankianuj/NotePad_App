541649c7288ce25e136a2109b508a9ca9ab76d21
solanki <anujaj2222.gmail.com>
2022-08-16 19:26:35 +0530

    Note Application

---
+++ notes/notesDTO/NotesDTO.java
@@ -0,0 +1,11 @@
+package com.example.notes.notesDTO;
+
+import lombok.Data;
+
+@Data
+public class NotesDTO {
+    private String note;
+    private String description;
+    private String labels;
+
+}
